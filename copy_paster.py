fab00f9d85f797bd8767c9486fed565897adb270
pythonistical <142655673+pythonistical@users.noreply.github.com>
2023-12-16 10:25:27 +0530

    Add files via upload

---
+++ copy_paster.py
@@ -0,0 +1,6 @@
+copy = input(str('Write what you want to copy\n'))
+copy_times = int(input('How many times do you want it to be copied?\n'))
+for i in range(copy_times):
+    print(copy)
+for i in range(copy_times):
+    print(copy,end = ' ')
