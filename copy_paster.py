dcae1ba0ebfc25f2a3fc3cf5c43ced8c11c98e9f
pythonistical <142655673+pythonistical@users.noreply.github.com>
2024-01-14 17:00:25 +0530

    Update copy_paster.py

--- copy_paster.py
+++ copy_paster.py
@@ -1,5 +1,5 @@
-copy = input(eval('Write what you want to copy\n'))
-copy_times = eval(input('How many times do you want it to be copied?\n'))
+copy = str(input('Write what you want to copy\n'))
+copy_times = int(input('How many times do you want it to be copied?\n'))
 for i in range(copy_times):
     print(copy)
 for i in range(copy_times):
